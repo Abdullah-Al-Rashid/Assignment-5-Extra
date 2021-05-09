54a6ac8423baf04ee907a73b6ad8cd3ec3a0e5d9
Abdullah0505 <>
2021-05-09 14:52:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+print('A quick and easy solution to finding the Area and Circumference of your Circle by inputting the diameter:')
+
+print()
+print('Trying to find out the Area of your Circle?')
+print()
+
+diameter = float(input('Enter the diameter of the Circle: '))
+area = math.pi * ((diameter * diameter)/4)
+print('Area of Circle is:', area)
+
+print()
+print('Trying to find out the Circumference of your Circle?')
+print()
+
+diameter = float(input('Enter the diameter of the Circle: '))
+circumference = 2 * math.pi * (diameter/2)
+print('Circumference of Circle is:', circumference)
